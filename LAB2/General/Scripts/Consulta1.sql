870ba821eeedbbefe1a2ff9ef38f2f7a9ec0113a
Nery Ezequiel Capo Brazeiro <nery.ezequiel.capo@fing.edu.uy>
2023-09-01 17:20:36 -0300

    arregloEstetico

--- LAB2/General/Scripts/Consulta1.sql
+++ LAB2/General/Scripts/Consulta1.sql
@@ -1,4 +1,10 @@
-SELECT  hotel_codigo, h.nombre FROM hoteles h, ciudades c ,paises p 
-WHERE c.nombre = h.nombre AND h.estrellas > 3 AND p.continente_codigo='EU'
-AND p.pais_codigo=c.pais_codigo; 
+SELECT  hotel_codigo, h.nombre FROM 
+	hoteles h, 
+	ciudades c ,
+	paises p 
+	WHERE 
+		c.nombre = h.nombre AND 
+		h.estrellas > 3 AND 
+		p.continente_codigo = 'EU' AND 
+		p.pais_codigo = c.pais_codigo; 
 
--- LAB2/General/Scripts/Consulta2.sql
+++ LAB2/General/Scripts/Consulta2.sql
@@ -1,5 +1,9 @@
 --PREGUNTAR SI CAMBIAR POR JOIN (el mismo cliente que realiza la reserva, figura la estadia)
 CREATE VIEW estadias AS SELECT 
-hotel_codigo, nro_habitacion, cliente_documento, check_in FROM
-estadias_anteriores ea NATURAL JOIN reservas_anteriores ra; 
+	hotel_codigo, 
+	nro_habitacion, 
+	cliente_documento, 
+	check_in 
+	FROM
+		estadias_anteriores ea NATURAL JOIN reservas_anteriores ra; 
 
