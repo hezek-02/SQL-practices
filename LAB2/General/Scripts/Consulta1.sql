211c776bc1564066a95b093a3c64c73454f5b17c
Nery Ezequiel Capo Brazeiro <nery.ezequiel.capo@fing.edu.uy>
2023-09-01 17:08:21 -0300

    borradorCOnsultas123

---
+++ LAB2/General/Scripts/Consulta1.sql
@@ -0,0 +1,4 @@
+SELECT  hotel_codigo, h.nombre FROM hoteles h, ciudades c ,paises p 
+WHERE c.nombre = h.nombre AND h.estrellas > 3 AND p.continente_codigo='EU'
+AND p.pais_codigo=c.pais_codigo; 
+
---
+++ LAB2/General/Scripts/Consulta3.sql
@@ -0,0 +1,40 @@
+--PREGUNTAR SI CAMBIAR POR NATURAL JOIN(el mismo cliente que realiza la reserva, figura la estadia)
+CREATE VIEW  reservas_anteriores_sin_precio AS SELECT 
+	ra.hotel_codigo,
+	ra.nro_habitacion,
+	ra.cliente_documento, 
+	ra.fecha_reserva, 
+	ra.check_in, 
+	check_out
+	FROM
+	estadias_anteriores ea JOIN reservas_anteriores ra 
+	ON
+		ea.hotel_codigo=ra.hotel_codigo	AND 
+		ea.nro_habitacion=ra.nro_habitacion AND 
+		ea.check_in=ra.check_in AND 
+		NOT EXISTS (
+			SELECT ra2.hotel_codigo,ra2.nro_habitacion,ra2.fecha_reserva FROM reservas_anteriores ra2 
+			WHERE
+				ra2.hotel_codigo =ea.hotel_codigo AND 
+				ea.nro_habitacion=ra2.nro_habitacion AND 
+				ra2.fecha_reserva> ra.fecha_reserva
+			)
+
+--Se ve los datos repetidos elimnados
+SELECT * FROM reservas_anteriores ra WHERE hotel_codigo IN (6461828,6463689);
+SELECT * FROM reservas_anteriores_sin_precio WHERE hotel_codigo IN (6461828,6463689);
+
+
+--Se ve la diferencia de conjuntos
+SELECT hotel_codigo,
+       nro_habitacion,
+       cliente_documento,
+       check_in 
+FROM estadias
+EXCEPT(
+SELECT hotel_codigo,
+       nro_habitacion,
+       cliente_documento,
+       check_in 
+FROM reservas_anteriores_sin_precio)
+
