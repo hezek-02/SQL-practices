327bc56bfe083120404336f3f8fe01417239a757
Nery Ezequiel Capo Brazeiro <nery.ezequiel.capo@fing.edu.uy>
2023-09-06 03:05:56 -0300

    añadoPruebas

--- LAB2/Scripts/Scripts/Consulta5.sql
+++
@@ -1,5 +0,0 @@
-SELECT c.cliente_documento, concat(c.nombre,' ',c.apellido) AS nombre_completo FROM
-	estadias e	NATURAL JOIN clientes c,
-	limitan l, hoteles h 
-	WHERE 
-		h.hotel_codigo = e.hotel_codigo AND l.pais1_codigo = c.pais_codigo AND l.pais2_codigo = h.pais_codigo 
--- LAB2/Scripts/Scripts/Consulta7.sql
+++
@@ -1,6 +0,0 @@
-SELECT ocupacion_codigo, descripcion, ROUND(AVG(check_out - check_in), 2)AS estadia_promedio FROM 
-	clientes c NATURAL JOIN reservas r NATURAL JOIN ocupaciones o 
-	GROUP BY (ocupacion_codigo,descripcion)
-
-
-
