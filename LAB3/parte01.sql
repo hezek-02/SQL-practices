3fcffeebc8e8e360f5104b674e21fd2e5871ef8b
Nery Ezequiel Capo Brazeiro <nery.ezequiel.capo@fing.edu.uy>
2023-10-10 11:16:08 -0300

    parte01

--- LAB3/parte01.sql
+++ LAB3/parte01.sql
@@ -5,25 +5,26 @@ DECLARE
     cantidad INTEGER;
 BEGIN
     -- Verificar si el cliente existe
-    IF NOT EXISTS (SELECT 1 FROM clientes WHERE doc = clientedoc) THEN
-        RAISE EXCEPTION 'No existe el cliente';
+    IF NOT EXISTS (SELECT 1 FROM clientes WHERE cliente_documento = clientedoc) THEN
+        RAISE NOTICE 'No existe el cliente';
     END IF;
 
     -- Calcular la cantidad de reservas o estadías según el código
     IF codigo = 'R' OR codigo = 'r' THEN
         SELECT COUNT(*) INTO cantidad
-        FROM reservas
-        WHERE cliente_doc = clientedoc
+        FROM reservas_anteriores
+        WHERE cliente_documento = clientedoc
         AND EXTRACT(YEAR FROM fecha_reserva) = anio;
     ELSIF codigo = 'E' OR codigo = 'e' THEN
         SELECT COUNT(*) INTO cantidad
-        FROM estadias
-        WHERE cliente_doc = clientedoc
+        FROM estadias_anteriores
+        WHERE cliente_documento = clientedoc
         AND EXTRACT(YEAR FROM check_in) = anio;
     ELSE
-        RAISE EXCEPTION 'Código de operación incorrecto';
+        RAISE NOTICE 'Código de operación incorrecto';
     END IF;
 
     RETURN cantidad;
 END;
-$$ LANGUAGE plpgsql;
+$$
+LANGUAGE plpgsql;
--- Scripts/principal.sql
+++ Scripts/principal.sql
@@ -1,4 +1,11 @@
-SELECT actividad_cliente('R', 123456789, 2023) AS cantidad_reservas;
+
+SELECT * FROM reservas_anteriores ra 
+SELECT * FROM estadias_anteriores ea  
+SELECT * FROM clientes   
+
+
+SELECT actividad_cliente('R', 81449752, 2011) ;
+SELECT actividad_cliente('e', 81449752, 2011) ;
 
 
 --DROP FUNCTION actividad_cliente(char, integer, integer);
