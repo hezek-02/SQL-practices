69077661ea2665ac6797ba870110e20c8870e145
Nery Ezequiel Capo Brazeiro <nery.ezequiel.capo@fing.edu.uy>
2023-10-11 02:02:28 -0300

    redefino parte01, no cambia el nombre de retorno de la func

--- LAB3/parte01.sql
+++ LAB3/parte01.sql
@@ -1,29 +1,29 @@
 -- Crear la función actividad_cliente
 CREATE OR REPLACE FUNCTION actividad_cliente(codigo CHAR(1), clientedoc INTEGER, anio INTEGER)
-RETURNS INTEGER AS $$--delimita rango de la funcion
-DECLARE
-    cantidad INTEGER;
+RETURNS INTEGER AS 
+$$--delimita rango de la funcion
 BEGIN
-	cantidad = 0;
     -- Verificar si el cliente existe
     IF NOT EXISTS (SELECT 1 FROM clientes WHERE cliente_documento = clientedoc) THEN
         RAISE NOTICE 'No existe el cliente';
     -- Calcular la cantidad de reservas o estadías según el código
     ELSIF codigo = 'R' OR codigo = 'r' THEN
-        SELECT COUNT(*) INTO cantidad
-        FROM reservas_anteriores
-        WHERE cliente_documento = clientedoc
-        AND EXTRACT(YEAR FROM fecha_reserva) = anio;
+        RETURN(
+	        SELECT COUNT(*) FROM reservas_anteriores
+	        WHERE cliente_documento = clientedoc
+	        AND EXTRACT(YEAR FROM fecha_reserva) = anio
+	       );
     ELSIF codigo = 'E' OR codigo = 'e' THEN
-        SELECT COUNT(*) INTO cantidad
-        FROM estadias_anteriores
-        WHERE cliente_documento = clientedoc
-        AND EXTRACT(YEAR FROM check_in) = anio;
+        RETURN(
+	        SELECT COUNT(*)  FROM estadias_anteriores
+	        WHERE cliente_documento = clientedoc
+	        AND EXTRACT(YEAR FROM check_in) = anio
+	       );
     ELSE
         RAISE NOTICE 'Código de operación incorrecto';
     END IF;
 
-    RETURN cantidad;
+    RETURN NULL;
 END;
 $$
 LANGUAGE plpgsql;
--- LAB3/parte02.sql
+++ LAB3/parte02.sql
@@ -1,22 +1,21 @@
 CREATE OR REPLACE FUNCTION ingreso_extra(codhotel INTEGER, OUT tipohab SMALLINT, OUT monto NUMERIC(8,2))
-RETURNS SETOF RECORD AS 
+RETURNS SETOF RECORD AS --RECORD es un tipo de dato especial, NO es un nombre
 $$
-BEGIN
+BEGIN--Se puede plantear alternativa con FOR IN, y LOOP 
 	RETURN QUERY SELECT tipo_habitacion_codigo AS tipohab, SUM((check_out-check_in)*precio_noche) AS monto FROM
 		costos_habitacion ch  JOIN habitaciones h ON 
 		h.hotel_codigo = ch.hotel_codigo AND 
-		h.nro_habitacion = ch.nro_habitacion JOIN 
-		estadias_anteriores ea ON 
+		h.nro_habitacion = ch.nro_habitacion JOIN estadias_anteriores ea ON 
 		ea.hotel_codigo = ch.hotel_codigo  AND 
 		ea.nro_habitacion = ch.nro_habitacion WHERE
 			ea.hotel_codigo = codhotel AND 
-			ch.fecha_desde =
+			ch.fecha_desde =--Es la fecha mas cercana al CHECK-IN,la ultima fecha q identifica al precio_noche, alt: NOT EXISTS
 				(SELECT  MAX(fecha_desde) FROM costos_habitacion ch2 WHERE 
 					ch2.hotel_codigo = ch.hotel_codigo  AND 
 					ch2.nro_habitacion  = ch.nro_habitacion  AND 
 					ch2.fecha_desde <= ea.check_in
 					)
-			AND NOT EXISTS 
+			AND NOT EXISTS --NO existe reserva asociada
 				(SELECT 1 FROM 
 				reservas_anteriores ra WHERE
 				ea.hotel_codigo = ra.hotel_codigo AND 
