73a12fc9ad7617b864ea5aa735f146d791673518
Nery Ezequiel Capo Brazeiro <nery.ezequiel.capo@fing.edu.uy>
2023-10-11 17:46:44 -0300

    Update parte04.sql

--- LAB3/parte04.sql
+++ LAB3/parte04.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION control_costos() RETURNS TRIGGER AS $control_costos$
     BEGIN
-        IF (TG_OP = 'UPDATE' OR NEW.precio_noche IS NULL OR NEW.precio_noche <= 0) THEN
+        IF (TG_OP = 'UPDATE' AND (NEW.precio_noche IS NULL OR NEW.precio_noche <= 0)) THEN
             RAISE NOTICE ("La actualización no es correcta");
         ELSIF (TG_OP = 'DELETE' AND EXISTS (SELECT 1 FROM costos_habitacion ch JOIN estadias e ON e.hotel_codigo = ch.hotel_codigo AND 
         e.nro_habitacion = ch.nro_habitacion WHERE --join estadia costo hab
