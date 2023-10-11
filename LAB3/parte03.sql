a4808b60fed4552615ce83d7872829bb52400b05
Nery Ezequiel Capo Brazeiro <nery.ezequiel.capo@fing.edu.uy>
2023-10-11 13:04:25 -0300

    backup

--- LAB3/parte03.sql
+++ LAB3/parte03.sql
@@ -1,39 +1,31 @@
 CREATE OR REPLACE PROCEDURE generar_reporte()
-$$
+AS $$
+DECLARE 
+    pais bpchar(2);
+    cant_estrellas int2;--eq smallint
+    total_extra NUMERIC(10,2);
+   	parcial_extra NUMERIC(10,2);
+   	hotel int4;
 BEGIN
 	FOR pais IN
-        SELECT pais_codigo FROM paises;
+        SELECT pais_codigo  FROM paises
     LOOP
-	    
-        -- can do some processing here
-        RETURN NEXT r; -- return current row of SELECT
+		FOR cant_estrellas IN
+	        SELECT DISTINCT estrellas  FROM hoteles
+	    LOOP
+		    total_extra := 0;
+		    IF EXISTS (SELECT  1 FROM hoteles h	WHERE h.pais_codigo = pais AND h.estrellas = cant_estrellas) THEN 
+				FOR hotel IN
+			        SELECT  h.hotel_codigo FROM hoteles h WHERE h.pais_codigo = pais AND h.estrellas = cant_estrellas
+			    LOOP
+			    	SELECT SUM(monto) INTO parcial_extra FROM ingreso_extra(hotel);
+			    	total_extra := total_extra + parcial_extra;
+    			END LOOP;
+			END IF;
+		   	INSERT INTO resumen (pais_codigo, cant_estrellas, total_extra)--insertar los valores
+            	VALUES (pais, cant_estrellas, total_extra);
+	    END LOOP;
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
 	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-SELECT  * FROM resumen r 
-
-
-SELECT * FROM paises p 
-
-SELECT * FROM hoteles h 
-
-SELECT  p.pais_codigo,h.estrellas,ingreso_extra(h.hotel_codigo)  FROM  paises p JOIN hoteles h ON
-	p.pais_codigo = h.pais_codigo 
-	GROUP BY p.pais_codigo,h.estrellas;
-	
-SELECT  generar_reporte();
--- Scripts/principal.sql
+++ Scripts/principal.sql
@@ -43,12 +43,22 @@ SELECT  ea1.hotel_codigo,ea1.nro_habitacion,ea1.check_in,ea1.check_out  FROM
 				)
 		GROUP BY tipo_habitacion_codigo
 		
-SELECT * FROM ingreso_extra(6468804);
+SELECT * FROM ingreso_extra(6463689);
+SELECT * FROM ingreso_extra(6463694);
+SELECT * FROM ingreso_extra(6473369);
 
+SELECT  * FROM paises p 
+
+SELECT * FROM	hoteles h
+   		WHERE h.pais_codigo = 'AD'
 --parte03
 CREATE TABLE IF NOT EXISTS resumen (
     pais_codigo bpchar(2),
     cant_estrellas int2,--eq smallint
     total_extra NUMERIC(10,2)
 );
-    
+
+CALL  generar_reporte();
+
+SELECT * FROM resumen
+--DELETE FROM resumen r 
