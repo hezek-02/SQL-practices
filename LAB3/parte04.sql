c9890af6020209290d9c94677c4074c3f600958e
Nery Ezequiel Capo Brazeiro <nery.ezequiel.capo@fing.edu.uy>
2023-10-12 01:06:28 -0300

    permitir borrar/actulizar, siempre era return null, se añade un CU en principal para parte04

--- LAB3/parte04.sql
+++ LAB3/parte04.sql
@@ -2,8 +2,10 @@ CREATE OR REPLACE FUNCTION control_costos() RETURNS TRIGGER AS $control_costos$
     BEGIN
         IF (TG_OP = 'UPDATE' AND (NEW.precio_noche IS NULL OR NEW.precio_noche <= 0)) THEN
             RAISE NOTICE 'La actualización no es correcta';
-        ELSIF (TG_OP = 'DELETE' AND EXISTS (SELECT 1 FROM costos_habitacion ch JOIN estadias e ON e.hotel_codigo = OLD.hotel_codigo AND 
-        e.nro_habitacion = OLD.nro_habitacion WHERE --join estadia costo hab
+           	RETURN NULL;
+        ELSIF (TG_OP = 'DELETE' AND EXISTS (SELECT 1 FROM estadias e WHERE 
+        e.hotel_codigo = OLD.hotel_codigo AND 
+        e.nro_habitacion = OLD.nro_habitacion AND --join estadia costo hab
         OLD.fecha_desde = 	--identifica si la fecha desde
 				(SELECT  MAX(fecha_desde) FROM costos_habitacion ch2 WHERE 
 					ch2.hotel_codigo = e.hotel_codigo  AND 
@@ -11,8 +13,9 @@ CREATE OR REPLACE FUNCTION control_costos() RETURNS TRIGGER AS $control_costos$
 					ch2.fecha_desde <= e.check_in
 					) ) ) THEN
             RAISE NOTICE 'La operación de borrado no es correcta';
+            RETURN NULL;
         END IF;
-        RETURN NULL;
+        RETURN NEW;
     END;
 $control_costos$ LANGUAGE plpgsql;
 
